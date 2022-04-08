a894d54bbdd3a406d213e1a1ac564e5ac5801271
DeathMaroko <asikkoko@gmail.com>
2022-04-08 23:14:21 +0200

    Successfully complete the code.

--- Secret_Message.py
+++ Secret_Message.py
@@ -1,23 +1,42 @@
-import string
-xd = []
-xd[:0] = string.ascii_lowercase
-#print(xd)
+# Convert input to list
+Uncoded_message = input()
+list_1 = []
+list_1[:0] = Uncoded_message.lower()
 
-x = input()
-y = x.replace(" ", "")
-#print(y)
+# Decoding
+list_2 = []
+for char in list_1:
+    dec_char = ord(char)
+    list_2.append(dec_char)
 
-def Convert(string):
-    list1 = []
-    list1[:0] = string
-    list = list1[::-1]
-    return list
-z = Convert(y)
-#print(z)
+# Reversing character and encoding // len == 32 is 'space'
+list_3 = []
+count_1 = 0
+for len in list_2:
+    count = 0
+    if len == 32:
+        list_3.append(chr(len))
+    for DEC in range(97, 123):       
+        if DEC == list_2[count_1]:            
+            if DEC < 112:
+                DEC = 122 - count
+                list_3.append(chr(DEC))
+                break
+            if DEC >= 112:
+                DEC = 122 - count
+                list_3.append(chr(DEC))
+                break
+        count += 1
+    count_1 += 1
 
-count = 0
-for char in z:
-    if char == xd[count]:
-        print(char)
-    count += 1
+# Convert list to string
+def List_To_String(list):     
+    string = ""     
+    for element in list: 
+        string += element       
+    return string 
+
+# Print result
+print(List_To_String(list_3))
+
 

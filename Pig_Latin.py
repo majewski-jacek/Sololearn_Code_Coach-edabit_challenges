0e5b4dea75fb79e90ed570f02e2658a4563d63ef
DeathMaroko <asikkoko@gmail.com>
2022-04-09 20:12:58 +0200

    Deleting an unnecessary letter.

--- Pig_Latin.py
+++ Pig_Latin.py
@@ -1,7 +1,6 @@
 def Convert(sentence):
     for word in sentence:
-        ch_1 = word[0]
-        result = word[1:]  + word[0] + 'ay' + ' '
+        result = word[1:] + word[0] + 'ay' + ' '
         print(result, end="")
 
 Convert(input().split())

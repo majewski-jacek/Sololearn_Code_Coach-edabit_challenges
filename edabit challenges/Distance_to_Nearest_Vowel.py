cbbf261a8125ddc9e5a240f0d747dac341707bb4
DeathMaroko <asikkoko@gmail.com>
2022-04-17 20:07:18 +0200

    Adding a project that takes in a string and for each character, returns the distance to the nearest vowel.

---
+++ edabit challenges/Distance_to_Nearest_Vowel.py
@@ -0,0 +1,28 @@
+def distance_to_nearest_vowel(txt):
+    count = 0
+    vowel_places = []
+    Vowels = ['a', 'e', 'i', 'o', 'u']
+    
+    for i in txt:        
+        if i in Vowels:
+            vowel_places.append(count)
+        count += 1
+
+    count_2 = 0
+    result = []
+    
+    for i in txt:
+        distances_from_vowels = []        
+        for k in vowel_places:
+            distance = count_2 - k
+            if distance < 0:
+                distance *= -1                
+            distances_from_vowels.append(distance)
+
+        count_2 += 1
+        result.append(min(distances_from_vowels))
+    return result
+    
+print(distance_to_nearest_vowel(input()))
+
+# https://edabit.com/challenge/jWHkKc2pYmgobRL8R

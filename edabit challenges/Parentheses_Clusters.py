23c079b2d9b5c65ca04a84d157bb13bc94ae29b4
DeathMaroko <asikkoko@gmail.com>
2022-11-24 22:15:42 +0100

    Renaming and slight reassignment of variables

--- edabit challenges/Parentheses_Clusters.py
+++ edabit challenges/Parentheses_Clusters.py
@@ -1,17 +1,15 @@
 def split(txt):	
-    count_1 = 0
-    count_2 = 0
-    list_1 = []
+    count_1, count_2 = 0, 0    
+    result = []
 
     if txt == '':
-        return list_1
-       
+        return result
+         
     for i in txt:
         if count_1 == count_2 and count_1 != 0:
-            count_1 = 0
-            count_2 = 0
-            list_1.append(' ')
-            list_1.append(i)
+            count_1, count_2 = 0, 0    
+            result.append(' ')
+            result.append(i)
 
             if i == '(':
                 count_1 += 1
@@ -21,15 +19,14 @@ def split(txt):
 
         elif i == '(':
             count_1 += 1
-            list_1.append(i)
+            result.append(i)
 
         elif i == ')':
             count_2 += 1
-            list_1.append(i)
+            result.append(i)
     
-    str = ""
-    list_1 = (str.join(list_1))
-    result = list(list_1.split(" "))
+    result = "".join(result)
+    result = list(result.split(" "))
     return result
 
 # https://edabit.com/challenge/Fpymv2HieqEd7ptAq
